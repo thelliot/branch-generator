4cb1c9dd20ac9c94fd89667f1a547a4526397e9a
Elliot Goldsmith <egoldsmith@worth.systems>
2021-09-30 11:17:22 +0200

    feat: basic MVP

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="container mx-auto flex flex-col justify-center items-center h-screen">
+    <img alt="Vue logo" src="./assets/logo.png" class="w-50">
+    <form @submit.prevent="generateName" class="mt-6">
+      <select name="type" id="prefix" v-model="type" :class="typeClasses">
+        <option v-for="{ item } in types" :key="item">{{ item }}</option>
+      </select>
+      <select name="prefix" id="prefix" v-model="prefix" :class="prefixClasses">
+        <option v-for="{ item } in prefixes" :key="item">{{ item }}</option>
+      </select>
+      <input type="text" placeholder="123 my branch" v-model="branchName">
+      <button type="submit" class="ml-4 px-2 py-1 bg-green-400 hover:bg-green-500 transition rounded">Generate</button>
+    </form>
+    <div v-if="status" class="p-4">
+      <p class="select-all font-semibold" :class="typeClasses">
+        {{ status }}
+      </p>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const prefixes = [
+  { item: 'TTG', color: 'green' },
+  { item: 'GP', color: 'yellow' },
+  { item: 'FIN', color: 'purple' },
+]
+const types = [
+  { item: 'feature', color: 'green' },
+  { item: 'bug', color: 'red' },
+  { item: 'task', color: 'blue' },
+  { item: 'hotfix', color: 'yellow' },
+]
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      type: types[0].item,
+      prefix: prefixes[1].item,
+      branchName: '',
+      status: '',
+    }
+  },
+  computed: {
+    format() {
+      return this.branchName.trim().toLowerCase().split(' ').join('-')
+    },
+    prefixes() {
+      return prefixes
+    },
+    types() {
+      return types
+    },
+    typeClasses() {
+      return this.generateTextColors(this.types, this.type)
+    },
+    prefixClasses() {
+      return this.generateTextColors(this.prefixes, this.prefix)
+    },
+  },
+  methods: {
+    generateTextColors(array, matches, shade = '500') {
+      return array.map((arrayItem) => ({ [`text-${arrayItem.color.toLowerCase()}-${shade}`]: arrayItem.item === matches }))
+    },
+    generateName() {
+      if (!this.branchName.trim().length) return
+      const branch = `${this.type}/${this.prefix}-${this.format}`
+      this.$copyText(branch).then(() => this.status = 'successfully copied to clipboard')
+    },
+  },
 }
 </script>
 
@@ -21,6 +81,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import VueClipboard from 'vue-clipboard2'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueClipboard)
+app.mount('#app')
